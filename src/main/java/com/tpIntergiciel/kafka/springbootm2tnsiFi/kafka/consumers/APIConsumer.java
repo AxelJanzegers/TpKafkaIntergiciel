c7d21f29df29668d6e7c69f8cac32375c0fafa7f
Lilian C <doxed97@protonmail.com>
2021-01-07 11:09:41 +0100

    get_values_global fonctionnel

--- src/main/java/com/tpIntergiciel/kafka/springbootm2tnsiFi/kafka/consumers/APIConsumer.java
+++ src/main/java/com/tpIntergiciel/kafka/springbootm2tnsiFi/kafka/consumers/APIConsumer.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import com.google.gson.Gson;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.transaction.Transactional;
 
@@ -33,4 +36,6 @@ public class APIConsumer {
         globalService.createGlobal(json.getGlobal());
         countryService.createCountries(json.getCountries());
     }
+
+
 }
--- src/main/java/com/tpIntergiciel/kafka/springbootm2tnsiFi/kafka/controllers/Controller2.java
+++ src/main/java/com/tpIntergiciel/kafka/springbootm2tnsiFi/kafka/controllers/Controller2.java
@@ -1,10 +1,11 @@
 package com.tpIntergiciel.kafka.springbootm2tnsiFi.kafka.controllers;
 
 import com.tpIntergiciel.kafka.springbootm2tnsiFi.kafka.producers.Producer2;
+import com.tpIntergiciel.kafka.springbootm2tnsiFi.spring.entities.Global;
+import com.tpIntergiciel.kafka.springbootm2tnsiFi.spring.services.GlobalService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller2 {
@@ -15,6 +16,10 @@ public class Controller2 {
     public Controller2(Producer2 producer) {
         this.producer = producer;
     }
+
+    @Autowired
+    private GlobalService globalService;
+
     @PostMapping("/request")
     public void messageToTopic(@RequestParam("req") String command){
         this.producer.sendMessage(command);
